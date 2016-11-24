19f7ec6f543a8f27b90ac963eddfbe99094beb58
nirdeshdhami <nirdesh007dhami@gmail.com>
2016-11-24 10:22:45 +0545

    Add files via upload
    find out remainder

---
+++ remainder.c
@@ -0,0 +1,16 @@
+/*Name :Nirdesh Dhami
+Roll no:15
+Subject:programming fundamental
+Date:2016-11-22
+*/
+#include<stdio.h>
+        
+        int main(){
+		int n,rem;
+printf("Enter any nnumber:");
+scanf("%d",&n);
+
+   rem=n%10;
+printf("\n rem is=%d\n",rem);
+return 0;
+}
